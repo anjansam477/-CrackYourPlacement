4f153f25d17aafb1c8ced9e36e8b1a0f5df86b69
anjansam477 <66263628+anjansam477@users.noreply.github.com>
2022-05-17 20:37:55 +0530

    Update Find the Duplicate Number.py

--- DAY 1/Find the Duplicate Number.py
+++ DAY 1/Find the Duplicate Number.py
@@ -16,3 +16,20 @@ for i in range(0, n):
     nums.append(ele)
 print("Duplicate number : ")
 print(findDuplicate(nums))
+
+#Floyd's Cycle Detection method (shorter faster and efficient)
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
